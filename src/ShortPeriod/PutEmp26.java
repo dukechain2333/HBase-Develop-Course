5b44f30fff3e878f4811e74cc6d519b68893a045
ZHChain <chain_13918925958@outlook.com>
2022-01-12 20:58:16 +0800

    Finished Exp2

--- src/ShortPeriod/PutEmp26.java
+++ src/ShortPeriod/PutEmp26.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Objects;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -42,7 +41,7 @@ public class PutEmp26 {
             String[] ls = line.split(",");
             put = new Put(ls[0].getBytes());
             for (int i = 1; i < ls.length; i++) {
-                if (!Objects.equals(ls[i], "")) {
+                if (ls[i] != "") {
                     put.addColumn(cols[i - 1].split(":")[0].getBytes(), cols[i - 1].split(":")[1].getBytes(), ls[i].getBytes());
                 }
             }
