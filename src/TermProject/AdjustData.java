d720517da809165c6803f515c137446e556e5ba3
ZHChain <chain_13918925958@outlook.com>
2022-01-14 15:31:33 +0800

    Finished TermProject adjust data part.

---
+++ src/TermProject/AdjustData.java
@@ -0,0 +1,32 @@
+package TermProject;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class AdjustData {
+    Configuration conf;
+    Connection conn;
+
+    public AdjustData() throws IOException {
+        this.conf = HBaseConfiguration.create();
+        conf.set("hbase.rootdir", "hdfs://localhost:9000/hbase");
+        conf.set("hbase.zookeeper.quorum", "localhost");
+        this.conn = ConnectionFactory.createConnection(this.conf);
+    }
+
+    public void putData(String tableName, String rowKey, String columnFamily, String columnName, String value) throws IOException {
+        Table table = this.conn.getTable(TableName.valueOf(tableName));
+        Put put = new Put(Bytes.toBytes(rowKey));
+        put.addColumn(Bytes.toBytes(columnFamily), Bytes.toBytes(columnName), Bytes.toBytes(value));
+        table.put(put);
+        table.close();
+    }
+}
