45873d0ea50e8db631a831c7307fa6141ab4cfdf
ZHChain <chain_13918925958@outlook.com>
2022-01-14 14:39:25 +0800

    Finished TermProject create table part.

---
+++ src/TermProject/CreateTable.java
@@ -0,0 +1,56 @@
+package TermProject;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+public class CreateTable {
+    Admin admin;
+    Configuration conf;
+    Connection conn;
+
+    public CreateTable() throws IOException {
+        this.conf = HBaseConfiguration.create();
+        conf.set("hbase.rootdir", "hdfs://localhost:9000/hbase");
+        conf.set("hbase.zookeeper.quorum", "localhost");
+        this.conn = ConnectionFactory.createConnection(this.conf);
+        this.admin = conn.getAdmin();
+    }
+
+    public void createPlayerInfo() throws IOException {
+        String tableName = "PlayerInfo";
+        if (!this.admin.isTableAvailable(TableName.valueOf(tableName))) {
+            HTableDescriptor ht = new HTableDescriptor(TableName.valueOf(tableName));
+            ht.addFamily(new HColumnDescriptor("Followings"));
+            this.admin.createTable(ht);
+        }
+        System.out.println("PlayerInfo created successfully");
+    }
+
+    public void createGameInfo() throws IOException {
+        String tableName = "GameInfo";
+        if (!this.admin.isTableAvailable(TableName.valueOf(tableName))) {
+            HTableDescriptor ht = new HTableDescriptor(TableName.valueOf(tableName));
+            ht.addFamily(new HColumnDescriptor("Followers"));
+            this.admin.createTable(ht);
+        }
+        System.out.println("GameInfo created successfully");
+    }
+
+    public void createPlayingInfo() throws IOException {
+        String tableName = "PlayingInfo";
+        if (!this.admin.isTableAvailable(TableName.valueOf(tableName))) {
+            HTableDescriptor ht = new HTableDescriptor(TableName.valueOf(tableName));
+            ht.addFamily(new HColumnDescriptor("PlayingGames"));
+            this.admin.createTable(ht);
+        }
+        System.out.println("PlayingInfo created successfully");
+    }
+}
